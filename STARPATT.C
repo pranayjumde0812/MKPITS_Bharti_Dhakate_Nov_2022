4ca2d23f05be48f0f99c00f309d6f664bc57c96e
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2022-12-12 12:31:21 +0530

    star pattern in c
        *
       **
      ***
     ****
    *****

---
+++ STARPATT.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,k=1;
+clrscr();
+for(i=1;i<=5;i++)
+{
+for(j=5;j>=1;j--)
+{
+if(k>=j)
+printf("*");
+else
+printf(" ");
+}
+k++;
+printf("\n");
+}
+getch();
+}
