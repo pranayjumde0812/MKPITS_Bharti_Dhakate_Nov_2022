8ba46040a667c18e3f232353d4560b0c809cb8f1
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2022-12-24 20:04:24 +0530

    sum of 2 digit where sum shows only 2 digit after decimal
    ex : 3.450+2.123=5.67(only two digit after decimal point)

---
+++ SUMF.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float i,s,n,m;
+clrscr();
+printf("enter two decimal number for addition");
+scanf("%f%f",&n,&m);
+s=n+m;
+printf("%.2f",s);
+getch();
+}
