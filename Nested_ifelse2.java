989f1d2b28457dd2b96c23c6144df7b65672cbb4
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-01-20 11:07:46 +0530

    Nested if-else example in java

---
+++ Nested_ifelse2.java
@@ -0,0 +1,22 @@
+class Nested_ifelse2
+{
+	public static void main(String[] args)
+	{
+		int age=20;
+		if(age<=40)
+		{
+			if(age<=15)
+			{
+				System.out.println("Child");
+			}
+			else
+			{
+				System.out.println("Young");
+			}
+		}
+		else
+		{
+			System.out.println("Old");
+		}
+	}
+}
