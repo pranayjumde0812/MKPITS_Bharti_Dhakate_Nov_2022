eb7c15a2f4822c399bafc7561a0f6374e4988ec4
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-01-20 11:05:06 +0530

    if else example in java

---
+++ If_else2.java
@@ -0,0 +1,16 @@
+class If_else2
+{
+
+public static void main(String[] args)
+{
+int time=15;
+if(time<12)
+{
+System.out.println("Good Morning");
+}
+else
+{
+System.out.println("good afternoon");
+}
+}
+}
