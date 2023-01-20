ba1ba453d6b3692087812ae16097ac9860ecf85d
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-01-20 11:22:13 +0530

    Pattern example in java
    *
          * *
         * * *
        * * * *
       * * * * *
      * * * * * *
     * * * * * * *

---
+++ Pattern1.java
@@ -0,0 +1,20 @@
+class Pattern1
+{
+  public static void main(String [] args)
+  {
+    int n=7;
+    for(int i=1; i<=n; i++)
+    {
+      for(int j=n-i; j>=1 ;j--)
+      {
+		 System.out.print(" ");
+	  }
+	  for(int k=1; k<=i; k++)
+	  {
+		 System.out.print(" *");
+	  }
+	  System.out.println();
+	}
+  }
+
+}
