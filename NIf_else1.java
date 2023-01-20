117ca3125572458ec934184471c923bccc8cdd8f
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-01-20 11:08:15 +0530

    Nested if-else in java

---
+++ NIf_else1.java
@@ -0,0 +1,18 @@
+class NIf_else1
+{
+
+public static void main(String[] args)
+{
+int time=19;
+if(time<12)
+{
+System.out.println("Good Morning");
+}
+else if(time>=12 && time<18)
+{
+System.out.println("good afternoon");
+}
+else
+System.out.println("good night");
+}
+}
