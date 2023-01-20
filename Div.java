eb9921bc4d62e273a214e78f13c01f40011b032d
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-01-20 10:58:30 +0530

    division of two numbers in java

---
+++ Div.java
@@ -0,0 +1,10 @@
+class Div
+{
+
+public static void main(String[] args)
+{
+ int a=89,b=6;
+float c=(float)a/b;      //you have to typecast to get value in decimal
+System.out.println("Division of a and b is "+c);
+}
+}
