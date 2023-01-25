2ee93d939dd02c35f1dca73d207fd7886f536f2b
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-01-25 14:17:25 +0530

    Assignment 19/1/2023
    Write a  program to check a given array of integers of length 1 or more and return true if the first element and the last element are equal in the given array.

---
+++ assignment_19_11.java
@@ -0,0 +1,24 @@
+/*Write a program to check two given integers whether either of them
+is in the range 100..200 inclusive.*/
+import java.util.Scanner;
+public class assignment_19_11
+{
+	public static boolean value()
+	{
+		Scanner sc=new Scanner(System.in);
+		boolean check = false;
+		System.out.println("Enter the two integer numbers");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a<=200 && b>=100 || a>=100 && b<=200)
+		{
+			check=true;
+		}
+		return check;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(value());
+	}
+}
