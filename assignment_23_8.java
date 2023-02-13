a9a48960df1780cc5ed2cb8879d493dd3051677f
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-02-13 21:30:42 +0530

    assignment 23 jan 2023 que 8
    Write a program to check three given integers and return true if one of them is 20 or more less than one of the others.

---
+++ assignment_23_8.java
@@ -0,0 +1,25 @@
+/* Write a program to check three given integers and return true
+if one of them is 20 or more less than one of the others */
+import java.util.Scanner;
+class assignment_23_8
+{
+	public static boolean logic()
+	{
+		Scanner sc=new Scanner(System.in);
+		boolean check=false;
+		System.out.println("Enter the three numbers : ");
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=sc.nextInt();
+		if(Math.abs(x-y)>=20 || Math.abs(y-z)>=20 || Math.abs(x-z)>=20)
+		{
+			check=true;
+		}
+		return check;
+	}
+	public static void main(String[] args)
+	{
+		System.out.println(logic());
+	}
+
+}
