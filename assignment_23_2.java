4388c0c3b7079f3ae1899e2f7303b6b95a2d56d1
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-02-13 21:25:00 +0530

    assignment 23 jan 2023 que 2
    Write a  program that accept two integers and return true if either one is 5 or their sum or difference is 5.

---
+++ assignment_23_2.java
@@ -0,0 +1,24 @@
+/* Write a program that accept two integers and return true if
+either one is 5 or their sum or difference is 5. */
+import java.util.Scanner;
+class assignment_23_2
+{
+	public static boolean value()
+	{
+		Scanner sc=new Scanner(System.in);
+		boolean check=false;
+		System.out.println("Enter the two integer numbers");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a==5 || b==5 ||(a+b)==5 || (a-b)==5)
+		{
+			check=true;
+		}
+		return check;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(value());
+	}
+}
