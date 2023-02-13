b7a6368a456c5df85b7b7a504d9c6bfd0d6a4f5e
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-02-14 00:22:04 +0530

    Add files via upload
    interface one --->    class two
    			\			/
    			 \		   /
    			class three

---
+++ interfacing2.java
@@ -0,0 +1,41 @@
+/* interface one     interface two
+			\			/
+			  \		   /
+			class three */
+
+
+
+interface one
+{
+	void dis();
+
+	void absmethod();
+}
+
+interface two
+{
+	void dis();
+
+	void absmethod();
+}
+
+class three implements one,two
+{
+	public void dis()
+	{
+	}
+	public void absmethod()
+	{
+		System.out.println("overridden function of abstract class one");
+	}
+
+}
+
+public class interfacing2
+{
+	public static void main(String[] args)
+	{
+		three ob=new three();
+		ob.absmethod();
+	}
+}
