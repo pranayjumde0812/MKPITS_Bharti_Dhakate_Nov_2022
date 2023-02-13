2abd5d003b1995935cbd5c286cecbb1654d93dc3
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-02-13 21:34:25 +0530

    assignment 23 jan 2023 que 13
    Write a program to rotate the elements of a given array of integers (length 4 ) in left direction and return the new array.

---
+++ assignment_23_13.java
@@ -0,0 +1,23 @@
+/* Write a program to rotate the elements of a given array of
+integers (length 4 ) in left direction and return the new array. */
+import java.util.Scanner;
+class assignment_23_13
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int[] arr=new int[4];
+		int i;
+		System.out.println("Enter the element of the array of length 4 : ");
+		for(i=0;i<4;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("New Array after rotation in left direction : ");
+		for(i=1;i<4;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println(arr[0]);
+	}
+}
