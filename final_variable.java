528cedbd958b113cfee9a6188291a53302483c6d
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-02-14 00:16:20 +0530

    use of "final" keyword for variable in java

---
+++ final_variable.java
@@ -0,0 +1,11 @@
+public class final_variable
+{
+	public static void main(String[] args)
+	{
+		final int a=2;  //1st use with normal variable
+		System.out.println(a);
+		//a=3;
+		System.out.println(a);
+	}
+}
+
