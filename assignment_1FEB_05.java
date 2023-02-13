5620a56588513e2473016b05327857da69803c26
bhartidhakate <119591149+bhartidhakate@users.noreply.github.com>
2023-02-13 23:26:23 +0530

    assignment 1 feb 2023 que 5
    Write a Java  program that takes an age (for example 20) as input and prints something as "You look older than 20".

---
+++ assignment_1FEB_05.java
@@ -0,0 +1,13 @@
+/*Write a Java  program that takes an age (for example 20) as input and
+prints something as "You look older than 20". */
+class assignment_1FEB_05
+{
+	public static void main(String[] args)
+	{
+		int age=20;
+		if(age==20)
+		{
+			System.out.println("You look older than 20");
+		}
+	}
+}
